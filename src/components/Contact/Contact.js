ea1655bfa227e159935fb60016339761dd24b0a9
Dylan Wang <97680620+dwang1257@users.noreply.github.com>
2024-06-15 16:04:09 -0400

    first commit

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.scss';
+
+const Contact = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,20 @@
+import './Projects.scss';
+
+const Projects = () => {
+    return (
+        <div>
+            <h1 className='projects-header'>Projects & Experience </h1>
+            <ul className='projects'>
+                <li className='project-one'>
+                    hello
+                </li>
+            </ul>
+        
+        </div>
+
+    )
+
+
+}
+
+export default Projects
